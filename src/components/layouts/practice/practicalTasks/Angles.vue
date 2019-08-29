e0708078d98a4ed75f7a09a81fdd80a986dbeaca
kingeugene <king.eugene1503@gmail.com>
2019-08-29 09:16:40 +0300

    new geometry

---
+++ src/components/layouts/practice/practicalTasks/Angles.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div>Колесо имеет 18 спиц. Углы между соседними спицами равны. Найдите угол,
+            который образуют две соседние спицы. Ответ дайте в градусах.</div>
+        <input type="text"  v-model="answer">
+        <p :class="{'messAnsOk': flagsText, 'messAnsError': !flagsText}">{{answerText}}</p>
+        <input type="submit" value="Ответить" @click="reply">
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                answer: "",
+                correctAnswer: 20,
+                answerText: "",
+            }
+        },
+
+        computed: {
+            flagsText() {
+                return (this.answerText === "Правильно");
+            }
+        },
+
+        methods: {
+            reply() {
+                this.answer == this.correctAnswer
+                    ?  this.answerText = "Правильно"
+                    : this.answerText = "Неправильно";
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
